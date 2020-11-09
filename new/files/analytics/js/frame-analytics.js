25f68b6a2fb4a5ba544bac85c503283e84587966
Sudistark <31372554+Sudistark@users.noreply.github.com>
2020-11-09 13:15:33 +0530

    Update frame-analytics.js

--- new/files/analytics/js/frame-analytics.js
+++ new/files/analytics/js/frame-analytics.js
@@ -2,4 +2,4 @@ console.log('Frame Analytics Script Securely Loaded');
 console.log('User Agent String: ' + navigator.userAgent);
 console.log('User Agent Vendor: ' + navigator.vendor);
 console.log('User OS: ' + navigator.platform);
-console.log('User Language: ' + navigator.language);
+console.log('User Language: ' + navigator.languagp);
