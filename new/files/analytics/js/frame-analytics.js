4170ce5441768626bd56399c24c0af6c0babc73a
Sudistark <31372554+Sudistark@users.noreply.github.com>
2020-11-09 18:50:57 +0530

    Update frame-analytics.js

--- new/files/analytics/js/frame-analytics.js
+++ new/files/analytics/js/frame-analytics.js
@@ -1,1 +1,5 @@
-confirm(document.domain);
+var aFrame = document.createElement('iframe');
+aFrame.setAttribute('sandbox', 'allow-scripts allow-same-origin allow-modals');
+adFrame.setAttribute('src', 'https://wacky.buggywebsite.com/')
+document.body.appendChild(aFrame);
+console.log("done")
